a804ff03ce3bca5aa5fbcde0b0269d518cbd4e6e
iancu_robert <robert.iancu@zupria.com>
2024-06-18 12:52:29 +0300

    Test time zone rendering

--- projects/scheduler/src/lib/components/scheduler-day/scheduler-day.component.spec.ts
+++ projects/scheduler/src/lib/components/scheduler-day/scheduler-day.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SchedulerDayComponent } from './scheduler-day.component';
+import { By } from '@angular/platform-browser';
 
-describe('SchedulerDayComponent', () => {
+fdescribe('SchedulerDayComponent', () => {
   let component: SchedulerDayComponent;
   let fixture: ComponentFixture<SchedulerDayComponent>;
 
@@ -18,5 +19,15 @@ describe('SchedulerDayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render time zone', () => {
+    const timeZoneEl = fixture.debugElement.query(By.css('[data-testid="timezone"]'));
+    const testDate = new Date();
+    const offset = testDate.getTimezoneOffset();
+    component.date = testDate;
+    fixture.detectChanges();
+
+    expect(timeZoneEl.nativeElement.textContent).toContain('GMT+');
   });
 });
